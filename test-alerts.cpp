7563ee987f8c2c3afbc123b0e180b0ec57134421
NaveenBalachandar-dev <77613377+NaveenBalachandar-dev@users.noreply.github.com>
2021-04-21 10:00:34 +0530

    Update test-alerts.cpp
    new test cases

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -6,3 +6,11 @@
 TEST_CASE("infers the breach according to limits") {
   REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
 }
+
+TEST_CASE("Passive Cooling system with Normal temp and O/P fed to controller") {
+	
+	BatteryCharacter batteryCharacter = {PASSIVE_COOLING,"TCQ_Tests"};
+	
+	checkAndAlert(TO_CONTROLLER, batteryCharacter, 30);
+	
+}
